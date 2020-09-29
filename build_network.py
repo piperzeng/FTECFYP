aba2471443564da17fb4bb6fe7c9d4c32385fa2e
piperzeng <hongni7@126.com>
2020-09-29 23:00:08 +0800

    Add 2nd degree model

--- build_network.py
+++ build_network.py
@@ -91,12 +91,18 @@ def preferentialAttachment_2ndOrder(max_nodes, max_edges, loner=False, max_p=1.0
 
     # max_p: specify a maximum number for p
     # because for first few nodes, this (G.degree(node) / G.number_of_edges()) ratio is very high
+    #           number of edges of node i + C * number of edges of i's neighbors
+    # We adopt p = ----------------------------------------------------------
+    #              number of edges in graph + C * sum of each node's degree^2
 
     # initialize empty graph
     G = nx.Graph()
     # initialize first two nodes and edge
     G.add_nodes_from([0, 1])
     G.add_edge(0, 1)
+    #coefficient
+    c = 0.3
+    print("Current coefficint c is %.2f" % c)
     for i in range(2, max_nodes):
         # from a list of existing nodes sorted by their degrees (descending order)
         # get their preference (prob. of forming edge with new node by dividing their degrees by total edges
@@ -106,22 +112,35 @@ def preferentialAttachment_2ndOrder(max_nodes, max_edges, loner=False, max_p=1.0
         print('---Inserted node %d---' % i)
         # print(node_degree_list)
         for node, degrees in node_list:
-            if (G.degree(node) / G.number_of_edges()) >= max_p:
+            # the sum of node's neighbors' edges
+            no_edge_neighbors = 0
+            for neighbors in nx.all_neighbors(G, node):
+                no_edge_neighbors += G.degree(neighbors)
+            # the sum of each node's degree^2
+            sum_2nd_degree = 0
+            for node1, degree1 in node_list:
+                sum_2nd_degree += pow(degree1, 2)
+            cal_p = (G.degree(node)+c*no_edge_neighbors)/(G.number_of_edges()+c*sum_2nd_degree)
+            if  cal_p >= max_p:
                 p = max_p
             else:
-                p = G.degree(node) / G.number_of_edges()
+                p = cal_p
             print("the p value for node %d is: %f" % (node, p))
             if random.random() <= p:
                 G.add_edge(node, i)
                 print('edge between %d and %d created' % (node, i))
+            if G.degree(i) >= max_edges:
+                break
         if not loner and G.degree(i) == 0:
             rand_node = np.random.randint(0, i - 1)
             G.add_edge(rand_node, i)
             print('did not form edge with prev. nodes, will add %d to rand. node %d' % (i, rand_node))
         # if max number of edges exceeded, end the program
-        if G.number_of_edges() >= max_edges:
-            print("exceeded max edges")
-            break
+
+        # if G.number_of_edges() >= max_edges:
+        #     print("exceeded max edges")
+        #     break
+
         # nx.draw(G, with_labels=True)
         # plt.show()
     nx.draw(G, with_labels=True)
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from build_network import *
 
 def main():
 
-    preferentialAttachPipi(50, 100)
-    preferentialAttachment(50, 100, loner=False, max_p=0.7)
+    # preferentialAttachPipi(50, 100)
+    # preferentialAttachment(50, 100, loner=False, max_p=0.7)
+    preferentialAttachment_2ndOrder(100, 6, max_p=0.8)
 
 if __name__ == "__main__":
     main()
