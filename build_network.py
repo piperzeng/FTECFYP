f1dc8a2df5464cb2fd2c371c577a229a0f1c45bd
SalmonTT <simontienz@gmail.com>
2020-10-27 14:58:42 +0800

    Update PA.md

--- build_network.py
+++ build_network.py
@@ -33,57 +33,42 @@ def preferentialAttachmentV1(max_nodes, loner=False):
                 G.add_edge(j, i)
         # ----- Part 1.3 -----
         if not loner & (G.degree[i] == 0):
-            # tmp = node_list[random.choice(node_list)]
-            tmp = node_list[0]
-            G.add_edge(tmp, i)
+            # rand_node = node_list[random.choice(node_list)]
+            # rand_node = node_list[0]
+            # G.add_edge(rand_node, i)
+            while(G.degree[i] == 0):
+                for j in node_list:
+                    if G.number_of_edges() != 0:
+                        p = G.degree(j) / (2 * G.number_of_edges())
+                    if (round(np.random.uniform(0, 1), 1) < p):
+                        G.add_edge(j, i)
     plotGraph(G)
     return (G)
 
 
-def preferentialAttachment(max_nodes, max_edges, loner=False, max_p=1.0):
-    # v_1 notes: this version follows the The Barabási–Albert model which uses a linear preferential attachment
-    # https://en.wikipedia.org/wiki/Barab%C3%A1si%E2%80%93Albert_model
-
-    # loner behavior == True: if the new node did not form an edge with prev. nodes,
-    # then the node will not form any edges
-
-    # max_p: specify a maximum number for p
-    # because for first few nodes, this (G.degree(node) / G.number_of_edges()) ratio is very high
-
-    # initialize empty graph
+def preferentialAttachmentV2(max_nodes, loner=False, max_p=1.0):
     G = nx.Graph()
-    # initialize first two nodes and edge
     G.add_nodes_from([0, 1])
     G.add_edge(0, 1)
+    # ----- Part 1 -----
     for i in range(2, max_nodes):
-        # from a list of existing nodes sorted by their degrees (descending order)
-        # get their preference (prob. of forming edge with new node by dividing their degrees by
-        # sum of degrees of all pre-existing edges
-
-        node_list = sorted(G.degree, key=lambda x: x[1], reverse=True)
-        # insert new node
+        # ----- Part 1.1 -----
+        node_list = sorted(node for (node, val) in sorted(G.degree, key=lambda x: x[1], reverse=True))
         G.add_node(i)
-        # print('---Inserted node %d---' % i)
-        # print(node_degree_list)
-        for node, degrees in node_list:
-            if (G.degree(node) / (2 * G.number_of_edges())) >= max_p:
+        # ----- Part 1.2 -----
+        for j in node_list:
+            if (G.degree(j) / (2 * G.number_of_edges())) >= max_p:
                 p = max_p
             else:
-                p = G.degree(node) / (2 * G.number_of_edges())
-            # print("the p value for node %d is: %f" % (node, p))
+                p = G.degree(j) / (2 * G.number_of_edges())
             if random.random() <= p:
-                G.add_edge(node, i)
-                # print('edge between %d and %d created' % (node, i))
-        if not loner and G.degree(i) == 0:
-            rand_node = np.random.randint(0, i - 1)
+                G.add_edge(j, i)
+        # ----- Part 1.3 -----
+        if not loner & (G.degree(i) == 0):
+            rand_node = node_list[random.choice(node_list)]
             G.add_edge(rand_node, i)
-            # print('did not form edge with prev. nodes, will add %d to rand. node %d' % (i, rand_node))
-        # if max number of edges exceeded, end the program
-        if G.number_of_edges() >= max_edges:
-            # print("exceeded max edges")
-            break
     plotGraph(G)
-
+    return(G)
 
 def preferentialAttachment_2ndOrder(max_nodes, max_edges, loner=False, max_p=1.0):
     # v2 Goals: Implement the "Two-level network model" (proposed by Dangalchev)
@@ -140,7 +125,7 @@ def preferentialAttachment_MDA(max_nodes, m0, m):
     # - current behavior: if total neighbors < m, use total neighbors
 
     # initialize a graph with m0 nodes connected in an arbitrary fashion
-    G = preferentialAttachment(m0, 100)
+    G = preferentialAttachmentV2(m0, 100)
     for new_node in range(m0 + 1, max_nodes):
         # first obtain a list of connected nodes in the existing graph
         connected_nodes_list = []
@@ -177,4 +162,4 @@ def preferentialAttachment_MDA(max_nodes, m0, m):
     return []
 
 
-preferentialAttachmentV1(100)
+preferentialAttachmentV2(100, loner=False, max_p=0.2)
--- markdown/PA.md
+++ markdown/PA.md
@@ -1,6 +1,9 @@
 # Description for the Preferential Attachment Functions
 
-## 1. PreferntialAttachmentV1
+## preferntialAttachmentV1
+
+This model follows the Barabási–Albert model which uses a linear preferential attachment. The detail can be found here: https://en.wikipedia.org/wiki/Barab%C3%A1si%E2%80%93Albert_model
+
 ```python
 def preferentialAttachmentV1(max_nodes, loner=False):
     G = nx.Graph()
@@ -63,8 +66,8 @@ Once we have calculated the probability, we used the numpy's random.uniform() fu
 
 ```python
 if not loner & (G.degree[i] == 0):
-    tmp = node_list[random.choice(node_list)]
-    G.add_edge(tmp, i)
+    rand_node = node_list[random.choice(node_list)]
+    G.add_edge(rand_node, i)
 ```
 
 This is the loner behavior of the graph. If $loner == False$ , $node_i$ will connect to a random existing node in the graph if it did not form an edge with $node_j$ in *Part 1.2*. We found that the method we use to select the random node has significant effect on the final graph. In the following section we will discuss the results of different selection methods using the following parameters:
@@ -73,31 +76,74 @@ This is the loner behavior of the graph. If $loner == False$ , $node_i$ will con
 preferentialAttachmentV1(100, False)
 ```
 
-#### Discussion of different random node selection methods for *Part 1.3*
+### Discussion of different random node selection methods for *Part 1.3*
 
-1. Randomly select a node from node_list using random.choice():
+#### 1. Randomly select a node from node_list using random.choice():
 
 ```python
 if not loner & (G.degree[i] == 0):
-    tmp = node_list[random.choice(node_list)]
-    G.add_edge(tmp, i)
+    rand_node = node_list[random.choice(node_list)]
+    G.add_edge(rand_node, i)
 ```
 
 The result from this method is shown below:
 
-![random.choice() method](./graphs/PAv1_graph1.PNG)
+![random.choice() method](../graphs/PAv1_graph1.PNG)
 
 The highlighted node in the middle is $node_0$ , and as you can see its degree isn't very different from other nodes (the size of the node represents its relative degree).
 
-2.  Simply choose the most connected node:
+#### 2.  Simply choose the most connected node:
 
 ```python
 if not loner & (G.degree[i] == 0):
-    tmp = node_list[0]
-    G.add_edge(tmp, i)
+    rand_node = node_list[0]
+    G.add_edge(rand_node, i)
 ```
 
-![random.choice() method](./graphs/PAv1_graph2.PNG)
+![Most connected node](../graphs/PAv1_graph2.PNG)
 
 The highlighted node is $node_0$ and its degree is significantly higher than other nodes. 
+
+#### 3. Repeat Part 1.2 until $G.degree[node_i] \ne 0$:
+
+```python
+while(G.degree[i] == 0):
+    for j in node_list:
+        if G.number_of_edges() != 0:
+            p = G.degree(j) / (2 * G.number_of_edges())
+        if (round(np.random.uniform(0, 1), 1) < p):
+            G.add_edge(j, i)
+```
+
+![repeat part 1.2](../graphs/PAv1_graph3.PNG)
+
+Note that the result from this method is similar to that of random.choice()
+
+## preferentialAttachmentV2()
+
+The second version of PA adds one parameter to the function: *max_p*.
+
+The codes are identical with the exception to the following part:
+
+#### Part 1.2 
+
+```python
+for node, degrees in node_list:
+    if (G.degree(node) / (2 * G.number_of_edges())) >= max_p:
+        p = max_p
+    else:
+        p = G.degree(node) / (2 * G.number_of_edges())
+        if random.random() <= p:
+           	G.add_edge(node, i)
+```
+
+We found that by setting the *max_p* to a low value (e.g. 0.2), no single node will have significantly higher degree than other nodes:
+
+```python
+preferentialAttachmentV2(100, loner=False, max_p=0.2)
+```
+
+The above function generated the following:
+
+![max_p = 0.2](../graphs/PAv2_graph1.PNG)
 
